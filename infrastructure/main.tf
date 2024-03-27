b94f2d01418c11775071e16aba76ba719920dc00
hasansivri <140492558+hasansivri@users.noreply.github.com>
2024-03-27 09:49:14 +0300

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,202 +23,92 @@ resource "aws_security_group" "mutual-sg" {
   vpc_id = data.aws_vpc.name.id
 
   ingress {
-    protocol = "tcp"
-    from_port = 30000
-    to_port = 32767
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 5000
-    protocol = "tcp"
-    to_port = 5000
+    protocol    = "tcp"
+    from_port   = 30000
+    to_port     = 32767
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
+  }
 
   ingress {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
   }
-
-resource "aws_security_group" "kube-worker-sg" {
-  name = var.sec-gr-k8s-worker
-  vpc_id = data.aws_vpc.name.id
-
 
   ingress {
-    protocol = "tcp"
-    from_port = 30000
-    to_port = 32767
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
   }
- ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 5000
-    protocol = "tcp"
-    to_port = 5000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
   ingress {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 5000
+    to_port     = 5000
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
   }
-
-  egress{
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "kube-worker-secgroup"
-  }
-}
-
-resource "aws_security_group" "kube-master-sg" {
-  name = var.sec-gr-k8s-master
-  vpc_id = data.aws_vpc.name.id
-
- 
-  ingress {
-    protocol = "tcp"
-    from_port = 30000
-    to_port = 32767
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 8080
-    protocol = "tcp"
-    to_port = 8080
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port = 5000
-    protocol = "tcp"
-    to_port = 5000
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  ingress {
-    protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress{
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cid
 
   egress {
-    protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  tags = {
-    Name = "kube-master-secgroup"
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0", "::/0"]
   }
 }
 
-resource "aws_iam_role" "ec2ecrfullaccess" {
-  name = "ecr_ec2_permission-${local.user}"
-  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"]
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
+resource "aws_iam_instance_profile" "example" {
+  name = "example-profile"
+  role = aws_iam_role.ec2ecrfullaccess.name
 }
+
 resource "aws_instance" "kube-master" {
-    ami = "ami-07d9b9ddc6cd8dd30"
-    instance_type = "t3a.medium"
-    iam_instance_profile = aws_iam_instance_profile.master-server-profile.name
-    vpc_security_group_ids = [aws_security_group.kube-master-sg.id, aws_security_group.mutual-sg.id]
-    key_name = "clarus"
-    subnet_id = "subnet-012b73e2614cfbe2b"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1a"
-    tags = {
-        Name = "kube-master"
-        Project = "tera-kube-ans"
-        Role = "master"
-        Id = "1"
-        environment = "dev"
-    }
+  ami                  = "ami-07d9b9ddc6cd8dd30"
+  instance_type        = "t3a.medium"
+  iam_instance_profile = aws_iam_instance_profile.example.name
+  vpc_security_group_ids = [aws_security_group.kube-master-sg.id, aws_security_group.mutual-sg.id]
+  key_name             = "clarus"
+  subnet_id            = "subnet-012b73e2614cfbe2b"  // select own subnet_id of us-east-1a
+  availability_zone    = "us-east-1a"
+  tags = {
+    Name        = "kube-master"
+    Project     = "tera-kube-ans"
+    Role        = "master"
+    Id          = "1"
+    environment = "dev"
+  }
 }
 
 resource "aws_instance" "worker-1" {
-    ami = "ami-07d9b9ddc6cd8dd30"
-    instance_type = "t3a.medium"
-    vpc_security_group_ids = [aws_security_group.kube-worker-sg.id, aws_security_group.mutual-sg.id]
-    key_name = "clarus"
-    subnet_id = "subnet-012b73e2614cfbe2b"  # select own subnet_id of us-east-1a
-    availability_zone = "us-east-1a"
-    tags = {
-        Name = "worker-1"
-        Project = "tera-kube-ans"
-        Role = "worker"
-        Id = "1"
-        environment = "dev"
-    }
+  ami                  = "ami-07d9b9ddc6cd8dd30"
+  instance_type        = "t3a.medium"
+  vpc_security_group_ids = [aws_security_group.kube-worker-sg.id, aws_security_group.mutual-sg.id]
+  key_name             = "clarus"
+  subnet_id            = "subnet-012b73e2614cfbe2b"  // select own subnet_id of us-east-1a
+  availability_zone    = "us-east-1a"
+  tags = {
+    Name        = "worker-1"
+    Project     = "tera-kube-ans"
+    Role        = "worker"
+    Id          = "1"
+    environment = "dev"
+  }
 }
 
-output kube-master-ip {
+output "kube-master-ip" {
   value       = aws_instance.kube-master.public_ip
   sensitive   = false
   description = "public ip of the kube-master"
 }
 
-output worker-1-ip {
+output "worker-1-ip" {
   value       = aws_instance.worker-1.public_ip
   sensitive   = false
   description = "public ip of the worker-1"
